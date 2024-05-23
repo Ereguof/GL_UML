58831c6082f0aacc92fc5c686640bfcff31f71c7
Bonnamour Pierre <pbonnamour@2D-LINUX-060.insa-lyon.fr>
2024-05-23 08:57:37 +0200

    test2

---
+++ main_pierre.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <Attribut.h>
+
+#include <string.h>
+
+
+int main(){
+
+	int attributId = 5;
+
+	int unite = 100;
+
+	string  description = "fjmdsljf";
+
+	Attribut Attribut1 = Attribut(int attributId, int unite, string description);
+
+
+}
