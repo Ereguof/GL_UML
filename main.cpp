000acd157684f5d1e7a19b5280cfeec4c70b233b
Ereguof <v0.rddos@gmail.com>
2024-05-23 10:18:25 +0200

    joris test lecture attributs

--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <Mesure.h>
-#include <Fournisseur.h>
-#include <Utilisateur.h>
-#include <Capteur.h>
-#include <Attribut.h>
+#include <list>
+#include "Mesure.h"
+#include "Fournisseur.h"
+#include "Utilisateur.h"
+#include "Capteur.h"
+#include "Attribut.h"
 
 
 typedef struct
@@ -22,6 +23,7 @@ typedef struct
 
 } Date;
 
+list < Attribut > listeAttributs;
 
 int lireDataset(string nomDossier)
 {
@@ -42,41 +44,36 @@ int lireDataset(string nomDossier)
     {
         string ligne;
         getline(rFluxAttribut, ligne);
+        getline(rFluxAttribut, ligne);
         if (rFluxAttribut)
         {
             int start;
             int end;
             
-            end = ligne.find(' ');
-            data.ip = ligne.substr(0, end);
+            end = ligne.find(';');
+            string attributID = ligne.substr(0, end);
 
             start = end+1;
-            end = ligne.find(' ', start);
-            data.userNameLog = ligne.substr(start, end-start);
+            end = ligne.find(';', start);
+            string unit = ligne.substr(start, end-start);
 
             start = end+1;
-            end = ligne.find(' ', start);
-            data.nameUser = ligne.substr(start, end-start);
+            end = ligne.find(';', start);
+            string description = ligne.substr(start, end-start);
 
-            start = end+2;
-            end = start+11;
-            data.date = ligne.substr(start, end-start);
+            // cout << "AttributID: " << attributID << " Unit: " << unit << " Description: " << description << endl;
+            Attribut attribut(attributID, unit, description);
+            listeAttributs.push_back(attribut);
+            //cout << "AttributID: " << listeAttributs.front().attributID << " Unit: " << listeAttributs.front().unit << " Description: " << listeAttributs.front().description << endl;
         }
     }
+    return 0;
 }
 
 
 int main()
 {
-    Date date;
-    date.annee = 2020;
-    date.mois = 12;
-    date.jour = 31;
-    date.heure = 23;
-    date.minute = 59;
-    date.seconde = 59;
-
-    cout << "Date: " << date.annee << "/" << date.mois << "/" << date.jour << " " << date.heure << ":" << date.minute << ":" << date.seconde << endl;
+    lireDataset("dataset");
 
     return 0;
 }
